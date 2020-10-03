eeaf312f776f3af4f0952601615823f366301420
dexterbulaong <dexter.j1.bulaong@gmail.com>
2020-10-02 19:38:35 -0800

    sql file exported from mysql

--- csce401_project_db.sql
+++ csce401_project_db.sql
@@ -1,15 +1,85 @@
-DROP DATABASE IF EXISTS `covid_project`;
-CREATE DATABASE `covid_project`; 
-USE `covid_project`;
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 03, 2020 at 05:35 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `covid_project`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `businesses`
+--
 
 CREATE TABLE `businesses` (
-	`business_id` int(11) NOT NULL,
-	`business_name` varchar(50) NOT NULL,
-    `business_address` varchar(50) NOT NULL,
-    `business_hours` varchar(50) NOT NULL,
-    `business_type` varchar(50) NOT NULL,
-    `business_link` varchar(50) NOT NULL,
-    `entry_date` date NOT NULL,
-    `last_updated` date NOT NULL,
-    PRIMARY KEY (`business_id`)
-    );
+  `business_id` int(11) NOT NULL,
+  `business_name` varchar(50) NOT NULL,
+  `business_address` varchar(50) NOT NULL,
+  `business_hours` varchar(50) NOT NULL,
+  `business_type` varchar(50) NOT NULL,
+  `business_link` varchar(50) NOT NULL,
+  `entry_date` date NOT NULL,
+  `last_updated` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `protocols`
+--
+
+CREATE TABLE `protocols` (
+  `business_id` int(11) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `mask_required` varchar(50) NOT NULL,
+  `customer_limit` varchar(50) DEFAULT NULL,
+  `curbside_pickup` varchar(50) DEFAULT NULL,
+  `curbside_delivery` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `businesses`
+--
+ALTER TABLE `businesses`
+  ADD PRIMARY KEY (`business_id`);
+
+--
+-- Indexes for table `protocols`
+--
+ALTER TABLE `protocols`
+  ADD PRIMARY KEY (`business_id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `protocols`
+--
+ALTER TABLE `protocols`
+  ADD CONSTRAINT `protocols_ibfk_1` FOREIGN KEY (`business_id`) REFERENCES `businesses` (`business_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
