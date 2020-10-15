b1fd39cae42192662d69733c5cc1424a4eed0134
dexterbulaong <dexter.j1.bulaong@gmail.com>
2020-10-15 13:11:30 -0800

    added DbOperations to perform the read and writes to database, still working on this

---
+++ covid_api/includes/DbOperations.php
@@ -0,0 +1,39 @@
+<?php
+
+    class DbOperations {
+
+        private $con;
+
+        function __construct(){
+            require_once dirname(__FILE__) . '/DbConnect.php';
+            $db = new DbConnect;
+            $this->con = $db->connect();
+        }
+
+        public function createUser($user_id, $user_email, $user_password, $first_name, $last_name){
+            if (!$this->emailExists($user_email)){
+                $stmt = $this->con->prepare("INSERT INTO users (user_email, user_password, first_name, last_name) VALUES (?,?,?,?)");
+                $stmt->bind_param("ssss", $user_email, $user_password, $first_name, $last_name);
+                if ($stmt->execute()) {
+                    return USER_CREATED;
+                } 
+                else {
+                    return USER_FAILURE;
+                }
+            }
+            return USER_EXISTS;
+        }
+            
+    
+        private function emailExists($user_email) {
+
+            $stmt = $this->con->prepare("SELECT user_id FROM users Where user_email = ?");
+            $stmt->bind_param("s", $user_email);
+            $stmt->execute();
+            $stmt->store_result();
+
+            return $stmt->num_rows > 0;
+
+        }
+    }
+?>
--- covid_api/public/index.php
+++ covid_api/public/index.php
@@ -6,21 +6,94 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-require __DIR__ . '/../includes/DbConnect.php';
+require __DIR__ . '/../includes/DbOperations.php';
+
 
 $app = AppFactory::create();
+/*added*/
+$app->setBasePath("/covid_api/public");
+$app->addRoutingMiddleware();
+$errorMiddleware = $app->addErrorMiddleware(true,true,true);
 
-$app->get('/covid_api/public/hello/{name}', function (Request $request, Response $response, array $args) {
-    $name = $args['name'];
-    $response->getBody()->write("Hello, $name");
+/* 
+    endpoint: createuser
+    parameters: user_email, user_password, first_name, last_name
+    method: POST
+*/
+$app->post('/createuser', function(Request $request, Response $response){
+    if(!haveEmptyParameters(array('user_email', 'user_password', 'first_name', 'last_name'), $response)) {
+        $request_data = $request->getParsedBody();
 
-    $db = new DbConnect;
+        $user_email = $request_data['user_email'];
+        $user_password = $request_data['user_password'];
+        $first_name = $request_data['first_name'];
+        $last_name = $request_data['last_name'];
 
-    if($db->connect() != null) {
-        echo 'Connection Successful';
+        $hash_password = password_hash($user_password, PASSWORD_DEFAULT);
+
+
+        $db - new DbOperations;
+
+        $result = $db->createUser($user_email, $hash_password, $first_name, $last_name);
+
+        if($result == USER_CREATED) {
+            $message = array();
+            $message['error'] = false;
+            $message['message'] = 'User created successfully';
+            /*added to line*/
+            $response->getBody()->write(json_encode($message));
+
+            return $response
+                        ->withHeader('Content-type', 'application/json')
+                        ->withStatus(201);
+        }
+        else if($result == USER_FAILURE) {
+            $message = array();
+            $message['error'] = true;
+            $message['message'] = 'Some error occurred';
+            /*added to line*/
+            $response->getBody()->write(json_encode($message));
+
+            return $response
+                        ->withHeader('Content-type', 'application/json')
+                        ->withStatus(422);
+        }
+        else if($result == USER_EXISTS) {
+            $message = arrray();
+            $message['error'] = true;
+            $message['message'] = 'User already exists';
+            /*added to line*/
+            $response->getBody()->write(json_encode($message));
+
+            return $response
+                        ->withHeader('Content-type', 'application/json')
+                        ->withStatus(422);
+        }
+
     }
-
-    return $response;
 });
 
+function haveEmptyParameters($required_params, $response) {
+    $error = false;
+    $error_params = '';
+    $request_params = $_REQUEST;
+
+    foreach($required_params as $param) {
+        if(!isset($request_params[$param]) || strlen($request_params[$param]) <= 0) {
+            $error = true;
+            $error_params .= $param . ', ';
+        }
+    }
+    if ($error) {
+        $error_detail = array();
+        $error_detail['error'] = true;
+        $error_detail['message'] = 'Required parameters ' . substr($error_params, 0, -2) . 'are missing or empty';
+        /*added to line*/
+        $response->getBody()->write(json_encode($error_detail));
+    }
+    return $error;
+}
+
 $app->run();
+
+?>
